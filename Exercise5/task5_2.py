1f746f0451cf8e4395b1a30249623506cb45b40d
klugh <klugh@student.ethz.ch>
2020-05-07 15:05:15 +0200

    task5_2

--- Exercise5/task5_2.py
+++ Exercise5/task5_2.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.special import gammaln
+import pandas as pd
 
+df = pd.DataFrame()
 mu = tau = 1
 N = 10
 eps = np.random.normal(mu, 1, N)
@@ -10,6 +12,7 @@ mu0 = 0
 lambda0 = 3
 a0 = 2
 b0 = 2
+s20 = b0 / (a0 * lambda0)
 
 
 def update_mu(a, b):
@@ -18,9 +21,10 @@ def update_mu(a, b):
     return s2, m
 
 
-def update_tau(a, b, m, s2):
-    a = a + (N + 1) / 2
-    b = b + 0.5 * np.sum((y - m) ** 2 + lambda0 * (mu0 - m) ** 2 + (N + lambda0) * s2)
+def update_tau(m, s2):
+    a = a0 + (N + 1) / 2
+    b = b0 + 0.5 * np.dot(np.transpose(y - np.ones(y.shape)), (y - np.ones(y.shape))) + lambda0 * (mu0 - m) ** 2 + (
+            N + lambda0) * s2
     return a, b
 
 
@@ -32,17 +36,28 @@ def evaluate_free_energy(a, b, s2):
 """
 Starting values
 """
-diff = free_energy = 1000
-a, b = a0, b0
-mu, s2 = mu0, b0 / (a0 * lambda0)
+for i in range(10):
+    diff = free_energy = 1000
+    if not i == 0:
+        a0 = np.random.randint(1, 10)
+        b0 = np.random.randint(1, 10)
+        mu0 = np.random.randint(0, 10)
+        s20 = np.random.randint(0, 10)
+    a, b = a0, b0
+    mu, s2 = mu0, s20
+    it = 0
+    while diff > 0.0001:
+        s2_new, mu_new = update_mu(a, b)
+        a_new, b_new = update_tau(mu, s2)
+        new_free_energy = evaluate_free_energy(a, b, s2)
+        diff = np.abs(free_energy - new_free_energy)
+        print(free_energy, new_free_energy, diff)
+        print(a_new, b_new, mu_new, s2_new)
+        free_energy = new_free_energy
+        a, b = a_new, b_new
+        mu, s2 = mu_new, s2_new
+        it += 1
 
-while diff > 0.001:
-    s2_new, mu_new = update_mu(a, b)
-    a_new, b_new = update_tau(a, b, mu, s2)
-    new_free_energy = evaluate_free_energy(a, b, s2)
-    diff = np.abs(free_energy - new_free_energy)
-    print(free_energy, new_free_energy, diff)
-    # print(a,b,m,s2)
-    free_energy = new_free_energy
-    a, b = a_new, b_new
-    mu, s2 = mu_new, s2_new
+    df = df.append(
+        pd.DataFrame([[a0, b0, mu0, s20, it, free_energy]], columns=['a0', 'b0', 'mu0', 's20', 'it', 'free_energy']))
+df.to_csv('results.csv', index = False)
